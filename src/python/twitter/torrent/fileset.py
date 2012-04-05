dfb8039c6cc841ecd6284e1267b79641d7092099
Brian Wickman <wickman@twitter.com>
2012-04-05 00:58:47 -0700

    that was a fucking pain in the ass
    (sapling split of bd698c68430428237e01de5ea193027063e7f05e)

--- src/python/twitter/torrent/fileset.py
+++ src/python/twitter/torrent/fileset.py
@@ -1,12 +1,24 @@
-import bisect
 import errno
 import hashlib
 import os
 import struct
+import sys
 import tempfile
 
 from twitter.common.dirutil import safe_mkdir, safe_rmtree
 
+def bisect_left(a, x, start=True, lo=0, hi=None):
+  if lo < 0:
+    raise ValueError('lo must be non-negative')
+  if hi is None:
+    hi = len(a)
+  while lo < hi:
+    mid = (lo+hi)//2
+    if (a[mid].start if start else a[mid].stop) < (x.start if start else x.stop):
+      lo = mid+1
+    else:
+      hi = mid
+  return lo
 
 # TODO(wickman) Do LRU filehandle caching here, since repeated open/close is going to be
 # costly.
@@ -70,22 +82,42 @@ class SliceSet(object):
     assert slyce.step is None  # only accept contiguous slices
 
     # find its spot
-    k = bisect.bisect_left(self._slices, slyce)
+    k = bisect_left(self._slices, slyce)
     self._slices.insert(k, slyce)
 
     # merge any overlapping slices
-    k = max(0, k - 1)
+    if k > 0 and self._slices[k-1].stop == slyce.start:
+      k = k - 1
     while k < len(self._slices) - 1:
       if self._slices[k].stop < self._slices[k + 1].start:
         break
       self._slices[k] = SliceSet._merge(self._slices[k], self._slices.pop(k + 1))
+
+  def missing_in(self, slyce):
+    def top_iter():
+      if len(self._slices) == 0:
+        yield slyce
+        return
+      L = max(0, bisect_left(self._slices, slyce) - 1)
+      R = bisect_left(self._slices, slyce, start=False)
+      yield slice(-sys.maxint,
+                  self._slices[L].start)
+      while L <= R and (L + 1) < len(self._slices):
+        yield slice(self._slices[L].stop, self._slices[L+1].start)
+        L += 1
+      yield slice(self._slices[max(L, len(self._slices)-1)].stop,
+                  sys.maxint if (L+1) >= len(self._slices) else self._slices[L+1].start)
+    for element in top_iter():
+      isect = slice(max(slyce.start, element.start), min(slyce.stop, element.stop))
+      if isect.stop > isect.start:
+        yield isect
 
   def __contains__(self, slyce):
     if isinstance(slyce, int):
       slyce = slice(slyce, slyce)
     if not isinstance(slyce, slice):
       raise ValueError('SliceSet.__contains__ expects an integer or another slice.')
-    k = bisect.bisect_left(self._slices, slyce)
+    k = bisect_left(self._slices, slyce)
     def check(index):
       return index >= 0 and len(self._slices) > index and (
           SliceSet._contains(slyce, self._slices[index]))
--- tests/python/twitter/torrent/test_sliceset.py
+++ tests/python/twitter/torrent/test_sliceset.py
@@ -32,15 +32,15 @@ def test_set_merge():
   assert ss.slices == [slice(0,3)]
 
 
-def test_multi_merge():  
+def test_multi_merge():
   ss = SliceSet()
   for k in range(-100, 100, 2):
     ss.add(slice(k, k+1))
   assert len(ss.slices) == 100
-  
+
   for k in range(50):
     assert slice(k, k+2) not in ss
-  
+
   ss.add(slice(0,100))
   assert len(ss.slices) == 51
   ss.add(slice(-100,100))
@@ -49,16 +49,57 @@ def test_multi_merge():
 
 def test_iter():
   ss = SliceSet()
-  
+
   counted = 0
   for sl in ss:
     counted += 1
   assert counted == 0
-  
+
   for k in range(0, 100, 2):
     ss.add(slice(k, k+1))
-  
+
   for sl in ss:
     counted += 1
   assert counted == 50
-  
+
+
+def test_big_missing():
+  ss = SliceSet()
+  for k in range(0, 20, 2):
+    ss.add(slice(k, k + 1))
+  assert list(ss.missing_in(slice(10, 30))) == [
+    slice(11, 12), slice(13, 14), slice(15, 16), slice(17, 18), slice(19, 30)]
+
+
+
+def test_missing():
+  ss = SliceSet()
+  assert list(ss.missing_in(slice(0, 100))) == [slice(0, 100)]
+
+  ss = SliceSet()
+  ss.add(slice(20, 40))
+
+  assert list(ss.missing_in(slice(0, 19))) == [slice(0, 19)]
+  assert list(ss.missing_in(slice(0, 20))) == [slice(0, 20)]
+  assert list(ss.missing_in(slice(0, 21))) == [slice(0, 20)]
+  assert list(ss.missing_in(slice(40, 60))) == [slice(40, 60)]
+  assert list(ss.missing_in(slice(39, 60))) == [slice(40, 60)]
+  assert list(ss.missing_in(slice(41, 60))) == [slice(41, 60)]
+  assert list(ss.missing_in(slice(20, 40))) == []
+  assert list(ss.missing_in(slice(20, 41))) == [slice(40, 41)]
+  assert list(ss.missing_in(slice(0, 60))) == [slice(0, 20), slice(40, 60)]
+
+  ss = SliceSet()
+  ss.add(slice(20, 29))
+  ss.add(slice(31, 40))
+
+  assert list(ss.missing_in(slice(0, 19))) == [slice(0, 19)]
+  assert list(ss.missing_in(slice(0, 20))) == [slice(0, 20)]
+  assert list(ss.missing_in(slice(0, 21))) == [slice(0, 20)]
+  assert list(ss.missing_in(slice(19, 30))) == [slice(19, 20), slice(29, 30)]
+  assert list(ss.missing_in(slice(40, 60))) == [slice(40, 60)]
+  assert list(ss.missing_in(slice(39, 60))) == [slice(40, 60)]
+  assert list(ss.missing_in(slice(41, 60))) == [slice(41, 60)]
+  assert list(ss.missing_in(slice(20, 40))) == [slice(29,31)]
+  assert list(ss.missing_in(slice(20, 41))) == [slice(29,31), slice(40, 41)]
+  assert list(ss.missing_in(slice(0, 60))) == [slice(0, 20), slice(29,31), slice(40, 60)]
